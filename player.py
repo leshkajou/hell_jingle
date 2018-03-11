b97967ddc50aed514574d79e18b815bcaba9f0b3
Vlad Markin <vlad.d.markin@gmail.com>
2018-03-11 18:12:35 +0300

    Initial commit

---
+++ player.py
@@ -0,0 +1,40 @@
+import time
+import mraa
+
+class Player:
+    def __init__(self, pin):
+        self._pwm_pin = mraa.Pwm(3)
+        self._volume = 0.3
+
+    def start(self):
+        print("Enable player")
+        self._pwm_pin.enable(True)
+        self._pwm_pin.write(0)
+
+    def stop(self):
+        print("Disable player")
+        self._pwm_pin.write(0)
+        self._pwm_pin.enable(False)
+
+    def set_volume(self, vol):
+        self._volume = vol
+
+    def play(self, melody):
+        print("Playing...")
+        self._pwm_pin.write(0)
+
+        for idx in range(len(melody)):
+            frequency, duration = melody[idx]
+
+            if frequency != 0:
+                period = int((10 ** 6) / frequency)
+                print("> {} : {} [{}]".format(frequency, duration, period))
+                self._pwm_pin.period_us(period)
+                self._pwm_pin.write(self._volume)
+            else:
+                print("> Mute for {}".format(duration))
+                self._pwm_pin.write(0)
+            time.sleep(duration)
+        self._pwm_pin.write(0)
+        print("Playing... Done!")
+
---
+++ test_play.py
@@ -0,0 +1,21 @@
+from player import Player
+
+p = Player(3)
+
+M = [
+    [261.63, 1],
+    [0, 0.5],
+    [329.63, 1],
+    [0,    0.5],
+    [329.63, 1],
+    [0, 0.5],
+    [523.25, 1],
+]
+
+p.start()
+p.play(M)
+#player.stop()
+
+
+
+
